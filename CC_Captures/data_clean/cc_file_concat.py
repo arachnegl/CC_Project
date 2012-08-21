eb5503ac20d9ac2b42ef4bdb7a0f12ba8e09048a
arachnegl <gregoryloyse@gmail.com>
2012-08-21 15:42:24 +0100

    uses native csv parser

--- CC_Captures/data_clean/cc_file_concat.py
+++ CC_Captures/data_clean/cc_file_concat.py
@@ -1,16 +1,20 @@
-import os, re
+import os, re, csv
 
 files = os.listdir('.')
 # discard all but .csv files:
 files = [f for f in files if re.findall(r".*.csv",f)]
 files = sorted(files) # timestamp in title ensures sorted in order
 
+
+
+
 dtype = [] # this will be populated with all [timestamp,watt] pairs
 
-for f in files:
-    date = f[:10]  # the date is the first 8 chars of filename
-    print(date)
-    with open(f,'r') as currFile:
-        for line in currFile:
-            if line[0][:8] != '00:00:0':
-                dtype.append([date + 'T'+ line[:8]]+[line[9:]])
+for fName in files:
+    fDate = f[:10]  # the date is the first 8 chars of filename
+    print(fDate)
+    with open(fName,'r') as openF:
+        reader = csv.reader(openF)
+        for line in reader:
+            line[0] = fDate + 'T' + line[0]
+            dtype.append(line)
