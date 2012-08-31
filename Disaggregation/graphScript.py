1a340aa1692079ec37e363e7569aa0daf33c54d8
arachnegl <gregoryloyse@gmail.com>
2012-08-31 19:17:46 +0100

    all functions now working fine and api nice

--- Disaggregation/graphScript.py
+++ Disaggregation/graphScript.py
@@ -3,6 +3,67 @@ This module provides functions for plotting the data
 
 """
 
+import matplotlib.pyplot as plt
+
+import dateutil.parser as dParser
+import matplotlib.dates as mplDates
+import datetime as dt
+
+import matplotlib as mpl
+
+
+def buildGraph(times,watts,name):
+    """
+    Function to build a graph
+    returns the figure object
+    """
+    fig = plt.figure()
+
+    graph = fig.add_subplot(111)
+    graph.plot_date(x=times,y=watts,fmt='r-')
+    graph.set_xlabel('Time')
+    graph.set_ylabel('Watts')
+    
+    graph.set_title(name)
+    graph.grid(True)
+    
+    # rotates and right aligns the x labels, and movees bottom of axes up to make room
+    fig.autofmt_xdate() #  bottom=0.18)    # adjust for date labels
+    # fig.subplots_adjust(left=0.18)
+    return fig
+
+
+def saveGraph(figObj,fName):
+    # maybe add some auto time in fName from figObj?
+    figObj.savefig(fName)
+
+
+def showGraph(figObj):
+    figObj.show()
+
+
+def zeroIndexTimesAxis(readings):
+    """
+    Zero indexes a list of times. 
+    Purpose: graph starts from time 0 instead of when readings started.  
+    (A better implementation would perhaps use min UTC but had probs with this.)
+
+    Function assumes and only makes sense if readings take place within a single day
+
+    >>>
+    >>>
+
+    """
+    time0 = readings[0][0]                                              # get first reading
+    d0 = time0.strftime(format=DATETIMEFORMAT)[:10] + "T00:00:00"  # set d0 to beg of day
+    d0 = dt.datetime.strptime(d0,DATETIMEFORMAT)
+    d1 = time0
+    delta = d1 - d0                 # amount to be subracted from each reading
+    return [[time[0] - delta, time[1]] for time in readings]
+
+
+
+
 """
 grill = readings[335:388]
 oven = readings[61:172]
@@ -28,71 +89,13 @@ plt.plot_date(x=grillTimes,y=grillWatts,xdate=True)
 plt.show()
 """
 
-import numpy as np
-import matplotlib.pyplot as plt
-import dateutil.parser as dParser
-import matplotlib.dates as mplDates
-import matplotlib as mpl
-import datetime as dt
-import matplotlib.pyplot as plt
-import re
-import cc_io
+"""
+# unneeded (for now) formatting code:
+timeFmt = mpl.dates.DateFormatter('%M:%S')  # formatting for x axis
+graph.xaxis.set_major_formatter(timeFmt)
+minLoc = mpl.dates.MinuteLocator()
+secLoc = mpl.dates.SecondLocator(interval=6)
+graph.xaxis.set_major_locator(minLoc)
+graph.xaxis.set_minor_locator(secLoc)
+"""
 
-def prepareData(fileName):
-    readings = getReadings(fileName)
-    readings = csvToList(readings)
-    readings = convertTimes(readings)
-    return readings
-
-def buildGraph(readings):
-    times = getTimes(readings)
-    watts = getWatts(readings)
-
-    # start building the graph:
-
-    fig = plt.figure()
-
-    graph = fig.add_subplot(111)
-    graph.plot_date(x=times,y=watts,fmt='r-')
-
-    timeFmt = mpl.dates.DateFormatter('%M:%S')  # formatting for x axis
-    graph.xaxis.set_major_formatter(timeFmt)
-    minLoc = mpl.dates.MinuteLocator()
-    secLoc = mpl.dates.SecondLocator(interval=6)
-    graph.xaxis.set_major_locator(minLoc)
-    graph.xaxis.set_minor_locator(secLoc)
-    
-    graph.set_xlabel('Time')
-    graph.set_ylabel('Watts')
-    
-    graph.set_title('Grill Readings')
-    plt.grid(True)
-    
-    # rotates and right aligns the x labels, and movees bottom of axes up to make room
-    fig.autofmt_xdate() #  bottom=0.18)    # adjust for date labels
-    # fig.subplots_adjust(left=0.18)
-
-
-readings = prepareData('grill.csv')
-buildGraph(readings)
-plt.savefig("grill.png")
-#plt.show()
-
-def zeroIndexTimesAxis(readings):
-    """
-    Zero indexes a list of times. 
-    Purpose: graph starts from time 0 instead of when readings started.  
-    (A better implementation would perhaps use min UTC but had probs with this.)
-
-    Function assumes and only makes sense if readings take place within a single day
-
-    >>>
-    >>>
-
-    """
-    time0 = readings[0][0]                                              # get first reading
-    d0 = time0.strftime(format=DATETIMEFORMAT)[:10] + "T00:00:00"  # set d0 to beg of day
-    d0 = dt.datetime.strptime(d0,DATETIMEFORMAT)
-    d1 = time0
-    delta = d1 - d0
-    return [[time[0] - delta, time[1]] for time in readings]
