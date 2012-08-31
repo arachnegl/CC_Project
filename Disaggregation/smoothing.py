f6d044fdc74bb5f847e4a2e2e2118bc22330c02c
arachnegl <gregoryloyse@gmail.com>
2012-08-31 13:02:29 +0100

    added new analysis functions

--- Disaggregation/smoothing.py
+++ Disaggregation/smoothing.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 
 def smooth(x,window_len=11,window='hanning'):
     """smooth the data using a window with requested size.
@@ -25,7 +25,7 @@ def smooth(x,window_len=11,window='hanning'):
     
     see also: 
     
-    numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
+    np.hanning, np.hamming, np.bartlett, np.blackman, np.convolve
     scipy.signal.lfilter
  
     TODO: the window parameter could be the window itself if an array instead of a string
@@ -47,20 +47,19 @@ def smooth(x,window_len=11,window='hanning'):
         raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
 
 
-    s=numpy.r_[x[window_len-1:0:-1],x,x[-1:-window_len:-1]]
+    s=np.r_[x[window_len-1:0:-1],x,x[-1:-window_len:-1]]
     #print(len(s))
     if window == 'flat': #moving average
-        w=numpy.ones(window_len,'d')
+        w=np.ones(window_len,'d')
     else:
-        w=eval('numpy.'+window+'(window_len)')
+        w=eval('np.'+window+'(window_len)')
 
-    y=numpy.convolve(w/w.sum(),s,mode='valid')
+    y=np.convolve(w/w.sum(),s,mode='valid')
     return y
 
 
 
-
-from numpy import *
+"""
 from pylab import *
 
 def smooth_demo():
@@ -96,7 +95,55 @@ def smooth_demo():
     legend(l)
     title("Smoothing a noisy signal")
     show()
+"""
+
+def getHist(valSrc):
+    """
+    Returns list of ints. Index of list are watt values. The values at each index are the total count of readings corresponding to that watt value.
+
+    >>> getHist([1,2,2,2])
+    [0, 1, 3]
+
+    # test case of unequal arrays + raise exception?
+    """
+    # generate zeroed array of appropriate size:
+    length = max(valSrc) + 1
+    histogram = [0 for i in range(length)]
+
+    for val in valSrc:
+        index = int(val) 
+        histogram[index] = histogram[index] + 1
+
+    return histogram
 
 
+
+def removePhantomEnergy(valList):
+    """
+    Returns 
+    """
+    histValList = getHist(valList)
+    minVal = histValList.index(max(histValList)) + 5
+
+    normalise = [x-minVal for x in valList]
+    normalise = [0 if x<0 else x for x in normalise]
+
+    return normalise
+
+
+def cleanSignal(watts):
+    """
+    Returns a smoothened and normalised signal.
+    """
+    watts = smooth(watts)
+    watts = removePhantomEnergy(watts)
+
+    return watts
+
+import doctest
+doctest.testmod()
+
+"""
 if __name__=='__main__':
     smooth_demo()
+"""
