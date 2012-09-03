af46eed3a2a567a1082ee2af087afe5d334e9b68
arachnegl <gregoryloyse@gmail.com>
2012-09-03 19:41:56 +0100

    code refactoring: added relevant funcs and merged with printApp.py

--- graphScript.py
+++ graphScript.py
@@ -1,29 +1,59 @@
 """
 This module provides functions for plotting the data
 
+
+
 """
 
+
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import datetime as dt
 
-DATETIMEFORMAT = '%Y-%m-%dT%H:%M:%S'
+import timeUtils as tu    # for zeroIndexTimesAxisMPL
+import appData as ap      # for app graphing func
 
 
-def buildGraph(times,watts,name):
+
+def getTimes(readings):
     """
-    Function to build a graph
-    returns the figure object
+    returns first column in list with 'two columns'
+
+    eg: [[a,b]] -> [a]
+
+    >>> a = [('12-08-12',345),('12-08-13',123)]
+    >>> getTimes(a)
+    ['12-08-12', '12-08-13']
+    """
+    return [r[0] for r in readings]
+
+
+def getWatts(readings):
+    """
+    returns list of watt values from second column of inputted list
+
+    [(a,b)] -> [b]
+
+    >>> a = [('12-08-12',345),('12-08-13',123)]
+    >>> getWatts(a)
+    [345, 123]
+    """
+    return [r[1] for r in readings]
+
+
+
+def getTimeWattFigure(times,watts,name):
+    """
+    Returns straightforward time series of watt values
+    
     """
     fig = plt.figure()
 
-    graph = fig.add_subplot(111)
-    graph.plot_date(x=times,y=watts,fmt='r-')
-    graph.set_xlabel('Time')
-    graph.set_ylabel('Watts')
+    axs = fig.add_subplot(111)
+    axs.plot_date(x=times,y=watts,fmt='r-')
+    axs.set_xlabel('Time')
+    axs.set_ylabel('Watts')
     
-    graph.set_title(name)
-    graph.grid(True)
+    axs.set_title(name)
+    axs.grid(True)
     
     # rotates and right aligns the x labels, and movees bottom of axes up to make room
     fig.autofmt_xdate() #  bottom=0.18)    # adjust for date labels
@@ -31,50 +61,99 @@ def buildGraph(times,watts,name):
     return fig
 
 
-def saveGraph(figObj,fName):
-    # maybe add some auto time in fName from figObj?
-    figObj.savefig(fName)
 
-
-def showGraph(figObj):
-    figObj.show()
-
-
-def zeroIndexTimesAxisDT(readings):
+def getAppFig():
     """
-    Zero indexes a list of times. 
-    Purpose: graph starts from time 0 instead of when readings started.  
-    (A better implementation would perhaps use min UTC but had probs with this.)
+    Returns figure showing all appliances
 
-    Function assumes and only makes sense if readings take place within a single day
+    cmds:
+    f = getAppFig1()
+    f.savefig('appliances.png')
 
-    >>>
-    >>>
-
+    (I know there is a better way of doing this)
     """
-    time0 = readings[0][0]                                              # get first reading
-    d0 = time0.strftime(format=DATETIMEFORMAT)[:10] + "T00:00:00"  # set d0 to beg of day
-    d0 = dt.datetime.strptime(d0,DATETIMEFORMAT)
-    d1 = time0
-    delta = d1 - d0                 # amount to be subracted from each reading
-    return [(time[0] - delta, time[1]) for time in readings]
+    # grill
+    grRs = ap.grill
+    grRs = tu.zeroIndexTimesAxisMPL(grRs)
+    grTs = getTimes(grRs)
+    grWs = getWatts(grRs)
 
-def zeroIndexTimesAxisMPL(readings):
-    """
-    Zero indexes a reading's list of mpl datetimes
-    """
-    dt0 = mdates.num2date(readings[0][0])
-    dt0 = dt0.isoformat()
-    dt0 = dt0[:10] + "T00:00:00"
-    dt0 = dt.datetime.strptime(dt0,DATETIMEFORMAT)
-    d1 = mdates.num2date(readings[0][0])
-    d1 = d1.replace(tzinfo=None)
-    # dt0.replace(tzinfo=None)
-    delta = d1 - dt0
-    # import pdb; pdb.set_trace()
-    results = [(mdates.num2date(r[0]).replace(tzinfo=None),r[1]) for r in readings]
-    results = [(r[0] - delta, r[1]) for r in results]
-    return [(mdates.date2num(r[0]),r[1]) for r in results]
+    # oven
+    ovRs = ap.oven
+    ovRs = tu.zeroIndexTimesAxisMPL(ovRs)
+    ovTs = getTimes(ovRs)
+    ovWs = getWatts(ovRs)
+    
+    # microwave
+    mwRs = ap.microwave
+    mwRs = tu.zeroIndexTimesAxisMPL(mwRs)
+    mwTs = getTimes(mwRs)
+    mwWs = getWatts(mwRs)
+
+    # tv
+    tvRs = ap.tv
+    tvRs = tu.zeroIndexTimesAxisMPL(tvRs)
+    tvTs = getTimes(tvRs)
+    tvWs = getWatts(tvRs)
+
+    # toaster
+    tsRs = ap.toaster
+    tsRs = tu.zeroIndexTimesAxisMPL(tsRs)
+    tsTs = getTimes(tsRs)
+    tsWs = getWatts(tsRs)
+
+    # washing machine
+    wmRs = ap.washingMachine
+    wmRs = tu.zeroIndexTimesAxisMPL(wmRs)
+    wmTs = getTimes(wmRs)
+    wmWs = getWatts(wmRs)
+
+    # dish washer
+    dwRs = ap.dishWasher
+    dwRs = tu.zeroIndexTimesAxisMPL(dwRs)
+    dwTs = getTimes(dwRs)
+    dwWs = getWatts(dwRs)
+
+
+    fig = plt.figure(figsize=None)
+
+    ax1 = fig.add_subplot(211)
+    ax1.set_title('Rectangular-like appliance signatures')
+    ax1.set_xlabel('Time')
+    ax1.set_ylabel('Watts')
+    ax1.xaxis_date(tz=None)
+
+    gr,ov,mw,tv = ax1.plot( grTs,grWs,'b-',    # rtrns 2D line
+                            ovTs,ovWs,'g-',
+                            mwTs,mwWs,'r-',
+                            tvTs,tvWs,'c-')
+
+    ax1.legend( (gr,ov,mw,tv),           # 2Dline handles
+                ('Grill','Oven','Microwave','Television'),
+                loc=('upper right'),
+                fancybox=True,
+                shadow=True)
+    
+    ax2 = fig.add_subplot(212)
+    ax2.set_title('Periodic-like  appliance signatures')
+    ax2.set_xlabel('Time')
+    ax2.set_ylabel('Watts')
+    ax2.xaxis_date(tz=None)
+
+    ts,wm,dw = ax2.plot( tsTs,tsWs,'m-',
+                         wmTs,wmWs,'g-',
+                         dwTs,dwWs,'r-')
+             
+    ax2.legend( (ts,wm,dw),           # 2Dline handles
+                ('Toaster','Washing Machine','Dishwasher'),
+                loc=('upper right'),
+                fancybox=True,
+                shadow=True)
+    
+    fig.subplots_adjust(hspace=0.5)
+    #fig.autofmt_xdate()   # erases x-axis of second graph
+    return fig
+
 
 
 """
@@ -86,4 +165,3 @@ secLoc = mpl.dates.SecondLocator(interval=6)
 graph.xaxis.set_major_locator(minLoc)
 graph.xaxis.set_minor_locator(secLoc)
 """
-
