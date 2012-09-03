81bf0b590a4b2eda1038c6ab19d6e6d3dea8fe25
arachnegl <gregoryloyse@gmail.com>
2012-09-03 20:41:28 +0100

    initial commit - convenience module for accessing appliance data

---
+++ appData.py
@@ -0,0 +1,27 @@
+"""
+Appliance data
+
+suggested usage:
+    import appData as ad
+
+"""
+
+import cc_io as io   # for getTimes and getWatts
+import graphScript as gs  # for zeroIndexTimesAxisMPL
+import matplotlib.pyplot as plt
+
+
+listOfApp = ['grill','oven','microwave',
+       'tv','washingMachine','dishWasher',
+       'toaster']
+
+appReadings = io.getReadingsFromFile('appliance_data_clean.csv')
+
+
+grill = appReadings[335:388]
+oven = appReadings[61:172]
+microwave = appReadings[652:671]
+tv = appReadings[723:841]
+washingMachine = appReadings[1092:1710]
+dishWasher = appReadings[1818:2097]
+toaster = appReadings[9:29]
