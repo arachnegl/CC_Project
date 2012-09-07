d6352fc593f849cbd6273b85f7d5db16ca69b946
arachnegl <gregoryloyse@gmail.com>
2012-09-07 08:27:43 +0100

    merging synth... -> timeutils

--- timeutils_cc.py
+++ timeutils_cc.py
@@ -95,3 +95,19 @@ def convertStrDateTimesToMPLDateTimes2(readings):
     return readings
 
 
+"""
+Various tools to synthesise days
+
+"""
+
+
+def dayRange():
+    start = dt.datetime(2012,01,01,0,0,0)  # take minum time as arbitrary beg
+    for n in range((60*60*24)/6):
+        yield start + dt.timedelta(seconds = 6*n)
+
+
+def getMPLDatesDayRange(interval):
+    start = dt.datetime(2012,01,01,0,0,0)
+    end = start + dt.timedelta(days=1)
+    return mdates.drange(start,end,dt.timedelta(seconds=6))
